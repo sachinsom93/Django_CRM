f5811c6e2eee55c06fa6a6eb585cd93ce6a57e0c
sachin <sachinsom507@gmail.com>
2021-04-17 16:34:54 -0700

    Added template dir

--- leads/admin.py
+++ leads/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Agent, User, Lead
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Agent)
+admin.site.register(Lead)
--- leads/models.py
+++ leads/models.py
@@ -11,9 +11,12 @@ class Lead(models.Model):
     age = models.IntegerField(default=0)
     email = models.EmailField(max_length=100)
     agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.user.email
 
